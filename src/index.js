ca2f5c76f771f387a33f35258b69788929285ee0
Mohil Zalavadiya <mohilnzalavadiya@gmail.com>
2020-07-05 12:48:48 +0530

    service worker path fix

--- src/index.js
+++ src/index.js
@@ -187,7 +187,7 @@ function onDocumentReady() {
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js');
+            navigator.serviceWorker.register('./service-worker.js');
         });
     }
 }
